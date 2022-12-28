16139998842a8e9735c0ec109981874b4b6a91e1
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-28 23:36:10 +0900

    Add users

--- db/init/users.sql
+++ db/init/users.sql
@@ -3,6 +3,19 @@ CREATE TABLE public.users (
   name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO public.users VALUES
-(1, '9sako6'),
-(2, 'JaneDoe');
+INSERT INTO public.users (name) VALUES
+('9sako6'),
+('JaneDoe'),
+('Miro'),
+('Jiro'),
+('Saburo'),
+('Hanako'),
+('Taro'),
+('Kite'),
+('John'),
+('Eric'),
+('Unity'),
+('Uribo'),
+('Yu'),
+('Cho'),
+('Alex');
